82bd79258b822f69dbe04f98f5fae7ead1384f30
Oliver Blass <oliverblass@web.de>
2024-09-03 09:33:42 +0200

    reorganized ContentView

---
+++ WeatherForecast/SampleMyWeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  SampleMyWeatherData.swift
+//  WeatherForecast
+//
+//  Created by Oliver Blass on 29.08.24.
+//
+
+import Foundation
---
+++ WeatherForecast/SevenDayForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  SevenDayForecastView.swift
+//  WeatherForecast
+//
+//  Created by Oliver Blass on 29.08.24.
+//
+
+import SwiftUI
+
+struct SevenDayForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SevenDayForecastView()
+}
